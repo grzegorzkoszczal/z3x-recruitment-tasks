7c2abdc7e661e1506769d4dc5c77910d118dc829
Grzegorz Koszczał <grzegorzkoszczal@gmail.com>
2024-05-15 21:07:06 +0200

    Simplified script - check data function removed, now blogs URLs are sorted based on date, from newest to oldest

--- main.py
+++ main.py
@@ -2,38 +2,15 @@ import sys
 import requests
 import re
 from bs4 import BeautifulSoup
-from datetime import date, timedelta
+from datetime import date, datetime, timedelta
 from collections import Counter
 from string import punctuation
 
 URL = "https://blog.hubspot.com/"
 
 
-def check_most_recent_blogs(check_older_blogs: int = 0) -> tuple[str, int]:
-    """
-    Purpose: 
-    Checks current date for finding the most recent blog entries.
-
-    Input:
-    check_older_blogs (int): In case there are not enough of most recent
-    blog entries, check the older ones.
-
-    Output:
-    Tuple consisting of two variables:
-    current_date_formated (str): String representation of current date.
-    check_older_blogs (int): Allows to change parameter.
-    """
-    current_date = date.today()
-    current_date = current_date - timedelta(days=check_older_blogs)
-    current_date_formated = current_date.strftime("%m/%d/%y")
-
-    return (current_date_formated, check_older_blogs)
-
-
 def fetch_blogs(
         number_of_articles_to_fetch: int,
-        current_date_formated: str,
-        check_older_blogs: int
     ) -> list[str]: 
     """
     Purpose:
@@ -43,9 +20,6 @@ def fetch_blogs(
     Input:
     number_of_articles_to_fetch (int): Variable to imply, how many articles we
     want to search for.
-    current_date_formated (str): String representation of current date.
-    check_older_blogs (int): In case there are not enough recent articles,
-    start looking for older ones.
 
     Output:
     URLs (list[str]): list of URLs of <number_of_articles_to_fetch>
@@ -54,25 +28,20 @@ def fetch_blogs(
     html_text = requests.get(URL).text
     soup = BeautifulSoup(html_text, "lxml")
     blogs = soup.find_all("li", class_="blog-categories-post")
-    URLs = list()
+    URLs_with_dates = list()
 
-    while number_of_articles_to_fetch != 0:
-        for blog in blogs:
-            if number_of_articles_to_fetch == 0:
-                break
-            posting_date_div = blog.div.div.div
-            posting_date = posting_date_div.find_all("p")[1].text.split()[0]
+    for blog in blogs:
+        posting_date_div = blog.div.div.div
+        posting_date = posting_date_div.find_all("p")[1].text.split()[0]
+        parsed_date = datetime.strptime(posting_date, "%m/%d/%y")
+        formatted_date = parsed_date.strftime("%d/%m/%y")
+        URL_to_blog = blog.div.div.h3.a["href"]
+        URLs_with_dates.append((URL_to_blog, formatted_date))
 
-            if posting_date in current_date_formated:
-                URL_to_blog = blog.div.div.h3.a["href"]
-                URLs.append(URL_to_blog)
-                number_of_articles_to_fetch -= 1
+    URLs_with_dates.sort(key=lambda date: datetime.strptime(date[1], "%d/%m/%y"), reverse=True)
+    URLs = [only_URLs[0] for only_URLs in URLs_with_dates]
 
-        check_older_blogs += 1
-        current_date_formated = check_most_recent_blogs(
-            check_older_blogs=check_older_blogs)[0]
-
-    return URLs
+    return URLs[:number_of_articles_to_fetch]
 
 
 def analyze_blogs(URLs: list[str], most_common: int) -> None:
@@ -119,13 +88,16 @@ def analyze_blogs(URLs: list[str], most_common: int) -> None:
             print(f"Blog URL: \"{url}\"")
             print(f"All words count: {len(words)}")
             print(f"All characters count: {len(text)}")
+            print("====================")
             print(f"Most frequent words in blog:")
             for word, count in most_common_words:
                 print(f'{word}: {count}')
+            print("====================")
 
             print(f"Most frequent keyphrases, consisting of two words in blog:")
             for bigram, count in most_common_bigrams:
                 print(f'{bigram}: {count}')
+            print("====================")
 
             print(f"Most frequent keyphrases, consisting of three words in blog:")
             for trigram, count in most_common_trigrams:
@@ -133,12 +105,7 @@ def analyze_blogs(URLs: list[str], most_common: int) -> None:
         
 
 def main(number_of_articles_to_fetch: int, most_common: int):
-    current_date_formated, check_older_blogs = check_most_recent_blogs()
-    URLs = fetch_blogs(
-        number_of_articles_to_fetch,
-        current_date_formated,
-        check_older_blogs
-    )
+    URLs = fetch_blogs(number_of_articles_to_fetch)
     analyze_blogs(URLs, most_common)
     
 
